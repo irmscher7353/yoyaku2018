89ed19aa2649b233223e0bf25e7d742cd392be6c
ruby@rails2 <von_mises@cosmo.plala.or.jp>
2017-12-24 17:18:35 +0900

    Prererence に self.to_hash を追加．

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,2 +1,10 @@
 class Preference < ApplicationRecord
+
+	def self.to_hash
+		h = {}
+		Preference.all.order(:updated_at).each do |preference|
+			h[preference.name] = preference.value
+		end
+		return h
+	end
 end
