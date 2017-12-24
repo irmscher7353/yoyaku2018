947b5bf4b5ac02d7f5e8f375a710f4e7bf4b1c92
ruby@rails2 <von_mises@cosmo.plala.or.jp>
2017-12-24 18:14:13 +0900

    rails g scaffold menu

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171221124142) do
+ActiveRecord::Schema.define(version: 20171224091127) do
+
+  create_table "menus", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "preferences", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
