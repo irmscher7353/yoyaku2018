b49c77c5d8e52bcbf0097b4a779ea6c7805f37fa
ruby@rails2 <von_mises@cosmo.plala.or.jp>
2018-01-20 21:16:50 +0900

    Menu.by_name の仕様変更と ordered の追加．

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,7 +2,15 @@ class Menu < ApplicationRecord
 	has_many :products
 	validates :name, presence: true, uniqueness: true
 
+	def self.by_name(name)
+		name.present? and where(name: name).first or latest
+	end
+
 	def self.latest
 		where(name: maximum(:name)).first
+	end
+
+	def self.ordered
+		order("name DESC")
 	end
 end
