0e1bd4e69332fd36446728fdccda0587f32eff56
ruby@rails2 <von_mises@cosmo.plala.or.jp>
2018-10-28 17:24:32 +0900

    orders/_form.html.erb で current_line_items を accepts_nested_attributes_for と field_for で表示できるようにしてみた．

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,4 +3,20 @@ class LineItem < ApplicationRecord
   belongs_to :product
 
 	paginates_per 15
+
+	def product_remain
+		product.present? ? product.remain : ''
+	end
+
+	def product_name
+		product.present? ? (product.title.present? ? product.title.name : '') : ''
+	end
+
+	def product_size
+		product.present? ? product.size : ''
+	end
+
+	def total_price_delimited
+		total_price.present? ? total_price.to_s(:delimited) : ''
+	end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,42 @@
 class Order < ApplicationRecord
   belongs_to :menu
   belongs_to :buyer
+	has_many :line_items
+	accepts_nested_attributes_for :line_items, allow_destroy: true
 
 	paginates_per 15
+
+	N_LINES = 5
+
+	def current_line_items
+		items = line_items.where(revision: revision).to_a
+		while items.count < N_LINES
+			items << line_items.build(revision: revision)
+		end
+		items
+	end
+
+	def due_year
+		due.present? ? due.localtime.year : (order.created_at or Time.zone.now).localtime.year
+	end
+
+	def due_month
+		due.present? ? due.localtime.month : 12
+	end
+
+	def due_day
+		due.present? ? due.localtime.day : ''
+	end
+
+	def due_wday
+		due.present? ? I18n.l(due.localtime, format: '%a') : ' '
+	end
+
+	def due_hour
+		due.present? ? due.localtime.hour : ''
+	end
+
+	def due_min
+		'%02d' % [due.present? ? due.localtime.min : 0 ]
+	end
 end
