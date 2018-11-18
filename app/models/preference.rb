36e63e21640fb3ed5b15008ac6ddafcbf39bee59
ruby@rails2 <von_mises@cosmo.plala.or.jp>
2018-11-18 17:20:04 +0900

    Preference に get_due_datetime と get_phrases メソッドを追加．

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,6 +1,23 @@
 class Preference < ApplicationRecord
 	validates :name, presence: true
 
+	def self.get_due_datetime
+		datetime = {}
+		h = to_hash
+		datetime[:monthes] = (h['引渡し開始月'].to_i .. h['引渡し終了月'].to_i).map{|mm| mm}
+		datetime[:days] = (h['引渡し開始日'].to_i .. h['引渡し終了日'].to_i).map{|dd| dd}
+		hh0, mm0 = h['引渡し開始時間'].split(':')
+		hh1, mm1 = h['引渡し終了時間'].split(':')
+		datetime[:hours] = (hh0.to_i .. hh1.to_i).map{|hh| hh}
+		datetime[:minutes] = [0, 30]
+		logger.info datetime
+		datetime
+	end
+
+	def self.get_phrases()
+		get_value('備考欄の常套句').split(/[\r\n]+/)
+	end
+
 	def self.get_value(name)
 		where(name: name).first.value
 	end
