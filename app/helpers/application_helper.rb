f81ccc5aa41e8ebfa571ad5a8baf2470f02ad97c
ruby@rails2 <von_mises@cosmo.plala.or.jp>
2018-11-18 17:19:27 +0900

    application_helper.rb で Array に fold メソッドを追加．

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
 end
+
+class Array
+	def fold(n)
+		result = Array.new
+		r = nil
+		self.each_index do |i|
+			p i
+			if (i % n) == 0
+				r = Array.new
+				result << r
+			end
+			r << self[i]
+		end
+		result
+	end
+end
