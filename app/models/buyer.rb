a8fcbca465f04e847efe943ce16e8da2d5178793
ruby@rails2 <von_mises@cosmo.plala.or.jp>
2018-12-29 17:42:23 +0900

    Buyer と Customer に self.unknown メソッドを追加．

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -2,4 +2,14 @@ class Buyer < ApplicationRecord
   belongs_to :customer
 
 	paginates_per 15
+
+	def self.unknown
+		customer = Customer.unknown
+		buyer = {
+			name: customer.name, phone: customer.phone, address: customer.address,
+			customer: customer,
+		}
+		(count <= 0) and create(buyer) or where(buyer).first
+	end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,10 @@
 class Customer < ApplicationRecord
 
 	paginates_per 15
+
+	def self.unknown
+		customer = { name: 'unknown', phone: '000-0000-0000', address: '', }
+		(count <= 0) and create(customer) or where(customer).first
+	end
+
 end
