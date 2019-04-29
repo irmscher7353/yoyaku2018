86bea55d2b3d9c2b0bc53d890a6a17526163b069
ruby@rails2 <von_mises@cosmo.plala.or.jp>
2019-04-29 10:54:08 +0900

    titles/index で priority を選択式に．

--- app/models/title.rb
+++ app/models/title.rb
@@ -5,6 +5,15 @@ class Title < ApplicationRecord
     greater_than_or_equal_to: 100, less_than_or_equal_to: 900,
   }
   has_many :products
+
+  Priorities = {
+    '100 - 定番デコ(いちご)' => 100,
+    '200 - 定番デコ(チョコ)' => 200,
+    '500 - 年替り' => 500,
+    '800 - ミール' => 800,
+    '900 - その他' => 900,
+  }
+
   after_initialize do |title|
     title.priority ||= 500
   end
